b2d0f9b2c902f478086acb86209487d7af861a82
Harshitaa <143587481+imharshitaa@users.noreply.github.com>
2025-12-19 16:12:07 +0530

    Update recon.py

--- core/recon.py
+++ core/recon.py
@@ -1,12 +1,35 @@
-ENGINE
+"""
+recon.py
+---------
+Performs lightweight reconnaissance:
+- Detect server type
+- Detect technologies via headers
+- Check HTTPS enforcement
+- Identify interesting server info
+"""
 
-subdomains
+class Recon:
+    def __init__(self, utils):
+        self.utils = utils
 
-ports
+    def analyze_headers(self, url):
+        resp = self.utils.http_request(url)
+        if not resp:
+            return {}
 
-tech stack
+        info = {
+            "server": resp.headers.get("Server", "Unknown"),
+            "powered_by": resp.headers.get("X-Powered-By", "Unknown"),
+            "security_headers": {
+                "Strict-Transport-Security": resp.headers.get("Strict-Transport-Security"),
+                "X-Content-Type-Options": resp.headers.get("X-Content-Type-Options"),
+                "Content-Security-Policy": resp.headers.get("Content-Security-Policy"),
+                "X-Frame-Options": resp.headers.get("X-Frame-Options")
+            }
+        }
 
-DNS
+        return info
+
 
 
 
